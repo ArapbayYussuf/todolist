49f353b2a42800ba1cf5ae75ecee49eebb2cfe11
ArapbayYussuf <arapbaijusip004@gmail.com>
2025-05-02 01:07:25 +0500

    01.05 finally

--- todolist/urls.py
+++ todolist/urls.py
@@ -14,6 +14,23 @@ Including another URLconf
     1. Import the include() function: from django.urls import include, path
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
+"""
+URL configuration for todolist project.
+
+The `urlpatterns` list routes URLs to views. For more information please see:
+    https://docs.djangoproject.com/en/5.2/topics/http/urls/
+Examples:
+Function views
+    1. Add an import:  from my_app import views
+    2. Add a URL to urlpatterns:  path('', views.home, name='home')
+Class-based views
+    1. Add an import:  from other_app.views import Home
+    2. Add a URL to urlpatterns:  path('', Home.as_view(), name='home')
+Including another URLconf
+    1. Import the include() function: from django.urls import include, path
+    2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
+"""
+
 from django.contrib import admin
 from django.urls import path, include
 from django.views.generic import TemplateView
@@ -22,10 +39,11 @@ from django.contrib.auth.views import LogoutView
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('api/', include('todo.urls')),
-    path('', TemplateView.as_view(template_name='index.html'), name='home'),
+    path('', TemplateView.as_view(template_name='landing.html'), name='landing'),
+    path('dashboard/', TemplateView.as_view(template_name='index.html'), name='dashboard'),
     path('login/', TemplateView.as_view(template_name='login.html'), name='login'),
     path('register/', TemplateView.as_view(template_name='register.html'), name='register'),
-    path('logout/', LogoutView.as_view(), name='logout'),
+    path('logout/', LogoutView.as_view(next_page='/'), name='logout'),
     path('profile/', TemplateView.as_view(template_name='profile.html'), name='profile'),
     path('tasks/', TemplateView.as_view(template_name='task_list.html'), name='task_list'),
     path('tasks/create/', TemplateView.as_view(template_name='task_create.html'), name='task_create'),
